c97a4f380ed2d425d817bd7d50865e621bec8384
DENISLMO <denislucianwork@protonmail.com>
2025-06-13 16:27:46 +0200

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup,find_packages
 
 setup(
     name="mi_conversor",
-    version="1.0",
+    version="0.1.0",
     description="Libreria sencilla de conversión",
     author="Denis Lucian Morar Ochis",
     package_dir={"":"src"},
-    packages=find_packages(where='src'),
+    packages=find_packages(where="src"),
     python_requires=">=3.7"
     
 )
